189b889845c380242d578a778897e3e55c06d110
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2022-06-10 22:31:09 +0100

    resultsnfixturesupdated

--- src/components/fixturesresults/Results.js
+++ src/components/fixturesresults/Results.js
@@ -9,11 +9,11 @@ function Results() {
     <div id="homeabout" className="uk-child-width-1-1@s" uk-grid>
       <div className="fixturescontainer">
         <div id="elevate" className="uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-text-center">
-          <h3 id="abouttitle"className="uk-text-lead">RESULTS</h3>
+          <h3 id="fixturetitle"className="uk-text-lead">RESULTS</h3>
           <br></br>
           <div>
           <iframe
-className="fixturesgrid" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR8CHpmypq_pEujTIftL9dbgjxwIftLYj8wMTyvFjP45nBrma-bR8WnW549jZkgkyi_-JULO3AXGBXv/pubhtml?gid=1819380640&amp;single=true&amp;widget=false&amp;headers=false&amp;scrolling=no" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0"></iframe>
+className="fixturesgrid" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR8CHpmypq_pEujTIftL9dbgjxwIftLYj8wMTyvFjP45nBrma-bR8WnW549jZkgkyi_-JULO3AXGBXv/pubhtml?gid=1079298692&amp;single=true&amp;widget=false&amp;headers=false&amp;scrolling=no&amp;chrome=false"></iframe>
           </div>
         </div>
       </div>     
