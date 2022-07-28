e954c5778e27ed0a422a7c803acc112574d0d87e
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2022-07-28 12:16:11 +0100

    new changes

--- src/components/common/Contact.js
+++ src/components/common/Contact.js
@@ -2,10 +2,95 @@
 function Contact() {
 
   return (
-    <div className="uk-section">
-      <div className="uk-container"></div>
-      <h3>Contact Us</h3>
-    </div>
+    <section>
+      <div id="homehero" className="uk-background-cover uk-height-large uk-panel uk-flex uk-flex-center uk-flex-middle">      
+      </div>
+      <div className="uk-child-width-1-1@s" uk-grid>
+        <div className="contactuscontainer">
+          <div id="contactus" className="uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-text-center">
+            <h3 id="contacttitle"className="uk-text-lead">EXECUTIVE COMMITTEE CONTACTS</h3>
+            <br></br>
+            <table className="uk-table">
+              <tbody>
+                <tr>
+                  <td className="paratext">President</td>	                            
+                  <td className="paratext">R.Bouchard</td>           
+                  <td className="paratext">richardbouch@aol.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Vice Presidents</td>	                      
+                  <td className="paratext">M.Le Mottee</td>          
+                  <td className="paratext">moandjulie@hotmail.co.uk</td>
+                </tr>
+                <tr>
+                  <td className="paratext"> </td> 	                                    
+                  <td className="paratext">G.Huelin</td>             
+                  <td className="paratext">graham.huelin@me.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext"> </td>
+                  <td className="paratext">G.Huelin</td>
+                  <td className="paratext">graham.huelin@me.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext"> </td>
+                  <td className="paratext">R.Pirouet</td>
+                  <td className="paratext">richard.pirouet60@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext"> </td>
+                  <td className="paratext">J.Bouchard</td>
+                  <td className="paratext">jonnybouchard@aol.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext"> </td>
+                  <td className="paratext">N. Le Masurier</td>
+                  <td className="paratext">nicklemas@hotmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Secretary</td>
+                  <td className="paratext">R. Marshall</td>
+                  <td className="paratext">marshallrobert46@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Treasurer</td>
+                  <td className="paratext">K.Armstrong</td>
+                  <td className="paratext">kjarmst@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Comp Secretary</td>
+                  <td className="paratext">J.Marshall</td>
+                  <td className="paratext">jamesmarshalljsy@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Air Rifle Secretary</td>
+                  <td className="paratext">Kirk Lewis</td>
+                  <td className="paratext">kirklewis27@me.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Assist Air Rifle Comp Secretary</td>
+                  <td className="paratext">Darren Fry</td>
+                  <td className="paratext">Darrenfry@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">.22 Sub Committee Chair</td>
+                  <td className="paratext">Simon Jelley</td>
+                  <td className="paratext">svjelley@gmail.com</td>
+                </tr>
+                <tr>
+                  <td className="paratext">Asst Comp Secretary</td>
+                  <td className="paratext">Steve Le Couilliard</td>
+                  <td className="paratext">stevecouilliard@outlook.com</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>     
+      </div>
+    </section>
   )
 }
 export default Contact
+
+
+
